3169fc461ad7b36b1362205327fafba58b94a8e3
Dani <dahhwe@gmail.com>
2023-03-05 21:52:26 +0700

    Optimization's, refactoring.

--- sys2/main.c
+++ sys2/main.c
@@ -3,88 +3,123 @@
 #include <pthread.h>
 #include <time.h>
 #include <windows.h>
+#include <stdbool.h>
 
 struct timespec ts;
-
-#define MAX_STUDENTS 10
-
-int N;
-int students_in_exam = 0;
-int students_waiting = 0;
+int results[100] = {0};
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t exam_finished = PTHREAD_COND_INITIALIZER;
+pthread_cond_t examFinished = PTHREAD_COND_INITIALIZER;
+
+int getInputInRange(int min, int max) {
+    int input;
+    char c;
+    bool valid = false;
+    while (!valid) {
+        printf("Введите целое число (%d - %d): ", min, max);
+        if (scanf("%d%c", &input, &c) == 2 && c == '\n' && input >= min && input <= max) {
+            valid = true;
+        } else {
+            printf("Некорректный ввод!в Попробуйте еще раз.\n");
+            while (getchar() != '\n'); // отчистка буфера
+        }
+    }
+    return input;
+}
+
 
 void *student(void *arg) {
-    int id = *(int *) arg;
+    int studID = *(int *) arg;
+    int maxNumOfStudAllowed = *(int *) arg;
+    int studentsInExam = 0;
+    int studentsWaiting = 0;
+
     pthread_mutex_lock(&mutex);
-    while (students_in_exam >= N) {
-        students_waiting++;
-        pthread_cond_wait(&exam_finished, &mutex);
-        students_waiting--;
+    while (studentsInExam >= maxNumOfStudAllowed) {
+        studentsWaiting++;
+        pthread_cond_wait(&examFinished, &mutex);
+        studentsWaiting--;
     }
-    students_in_exam++;
-    printf("Студент %d готовится сдачи экзамена\n", id);
+    studentsInExam++;
+    printf("Студент #%d готовится к сдаче экзамена\n", studID);
     pthread_mutex_unlock(&mutex);
 
-    srand(time(NULL) ^ id); // Перезагрузка генератора случайных чисел
+    srand(time(NULL) ^ studID); // Перезагрузка генератора случайных чисел
     int result = rand() % 3;
 
     ts.tv_sec = 1 + rand() % 5;
     ts.tv_nsec = 0;
     nanosleep(&ts, NULL);
 
+    printf("Студент #%d подходит сдавать работу\n", studID);
     pthread_mutex_lock(&mutex);
     if (result == 0) {
-        printf("\033[1;32mЭкзаменатор поставил студенту %d оценку %d\n\033[0m", id, rand() % 3 + 3);
+        printf("\033[1;32mЭкзаменатор поставил студенту #%d оценку %d\n\033[0m", studID, rand() % 3 + 3);
+        results[studID - 1] = 1; // Студент сдал экзамен
         ts.tv_sec = 1; // Добавление задержки между результатами каждого ученика
         ts.tv_nsec = 0;
     } else if (result == 1) {
-        printf("\033[1;31mЭкзаменатор пинками выгнал студента %d\n\033[0m", id);
+        printf("\033[1;31mЭкзаменатор выгнал студента #%d за списывание!\n\033[0m", studID);
+        results[studID - 1] = -1; // Студент не сдал экзамен
         ts.tv_sec = 1;
         ts.tv_nsec = 0;
     } else {
-        printf("\033[1;33mСтудент %d попробовал сдать работу экзаменатору и был отправлен "
-               "подумать за парту\n\033[0m", id);
+        printf("\033[1;33mЭкзаменатор отправил студента #%d подумать еще за парту\n\033[0m", studID);
         ts.tv_sec = 1;
         ts.tv_nsec = 0;
-        students_in_exam--;
-        pthread_cond_signal(&exam_finished);
+        studentsInExam--;
+        pthread_cond_signal(&examFinished);
         pthread_mutex_unlock(&mutex);
         student(arg);
         return NULL;
     }
-    students_in_exam--;
-    pthread_cond_signal(&exam_finished);
+    studentsInExam--;
+    pthread_cond_signal(&examFinished);
     pthread_mutex_unlock(&mutex);
-
 }
 
-int main(int argc, char **argv) {
-    SetConsoleOutputCP(CP_UTF8);
-    if (argc < 2) {
-        printf("Введите количество парт в аудитории: ");
-        scanf("%d", &N);
-    } else {
-        N = atoi(argv[1]);
+void printResults(int maxStudents) {
+    int numPassed = 0;
+    int numFailed = 0;
+    for (int i = 0; i < maxStudents; i++) {
+        if (results[i] == 1) {
+            numPassed++;
+        } else if (results[i] == -1) {
+            numFailed++;
+        }
     }
+    printf("╭────────────────────────────────────╮\n"
+           "│             Результаты             │\n"
+           "╰────────────────────────────────────╯\n");
+    printf("Студентов сдавших экзамен: %d\n", numPassed);
+    printf("Студентов проваливших экзамен: %d\n", numFailed);
+}
 
-    int students[MAX_STUDENTS];
-    pthread_t threads[MAX_STUDENTS];
+int main() {
+    int maxStudents;
+    SetConsoleOutputCP(CP_UTF8);
+    printf("Введите количество студентов сдающих экзамен\n");
+    maxStudents = getInputInRange(1, 100);
+    pthread_t threads[maxStudents];
 
-    // Цикл, позволяющий сдавать несколько экзаменов
+
     while (1) {
-        for (int i = 0; i < MAX_STUDENTS; i++) {
-            students[i] = i + 1;
-            pthread_create(&threads[i], NULL, student, &students[i]);
+        printf("╭────────────────────────────────────╮\n"
+               "│          Начало экзамена           │\n"
+               "╰────────────────────────────────────╯\n");
+        for (int i = 0; i < maxStudents; i++) {
+            int *id = malloc(sizeof(int));
+            *id = i + 1;
+            pthread_create(&threads[i], NULL, student, id);
         }
 
-        for (int i = 0; i < MAX_STUDENTS; i++) {
+        for (int i = 0; i < maxStudents; i++) {
             pthread_join(threads[i], NULL);
         }
 
-        // Предлагает пользователю начать другой экзамен
-        printf("\nНачать новый экзамен? (y/n): ");
+        printResults(maxStudents);
         char c;
+
+        printf("\nНачать новый экзамен? (y/n): ");
         scanf(" %c", &c);
 
         if (c != 'y' && c != 'Y') break;
